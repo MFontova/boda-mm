f576243067c0d31d8670201cfe52097d0f1b362e
Marc Fontova <mfontovat@gmail.com>
2025-03-21 20:58:56 +0100

    fix: remove Atropos component and adjust layout in AtroposCard

--- src/components/AtroposCard.tsx
+++ src/components/AtroposCard.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import Atropos from "atropos/react"
+// import Atropos from "atropos/react"
 import { StaticImageData } from "next/image"
 import Link from "next/link"
 
@@ -12,7 +12,7 @@ interface Props {
 
 export const AtroposCard = ({title, image, href}: Props) => {
   return (
-    <Atropos className="w-full" duration={200} >
+    <div className="w-full" >
       <Link href={href} style={
         {
           backgroundImage: `url(${image.src})`,
@@ -21,8 +21,8 @@ export const AtroposCard = ({title, image, href}: Props) => {
           backgroundRepeat: 'no-repeat',
         }
       } className="w-full h-52 flex justify-center items-center rounded-lg bg-black/50 bg-blend-overlay">
-        <h3 className="text-4xl font-bold text-white text-center" data-atropos-offset="10"> {title} </h3>
+        <h3 className="text-4xl font-bold text-white text-center" > {title} </h3>
       </Link>
-    </Atropos>
+    </div>
   )
 }
