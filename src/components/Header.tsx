ffa6fddca1b7b26166c9b627662c04cf082fb616
Marc Fontova <mfontovat@gmail.com>
2025-03-22 09:09:13 +0100

    fix: update Header component to include logo and adjust layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,15 @@
+import Link from "next/link"
 import { Menu } from "./Menu"
+import Image from "next/image"
+import logo from "@/../public/images/logo.svg"
 // import { MenuLink } from "./MenuLink"
 
 export const Header = () => {
   return (
-    <header className="flex justify-evenly gap-5 md:gap-10 md:justify-center absolute top-0 w-full text-xl font-bold">
-      {/* <MenuLink href="/" title="Inici" />
-      <MenuLink href="/where" title="El lloc" />
-      <MenuLink href="/chronology" title="Cronograma" />
-      <MenuLink href="/confirm" title="Confirmar assistència" /> */}
+    <header className="flex justify-evenly items-center gap-5 md:gap-10 md:justify-center absolute top-0 w-full text-xl font-bold">
+      <Link href="/" className="m-3">
+        <Image src={logo} alt="Mariona i Marc" style={{filter: "invert(1)"}} height={75} />
+      </Link>
       <Menu/>
     </header>
   )
